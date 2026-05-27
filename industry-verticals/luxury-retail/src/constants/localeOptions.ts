2d41f26e14cf442045bc4401750955b7a2f6c547
Mehmet Vanli <mehmet.vanli@gmail.com>
2026-05-27 15:51:19 +1000

    Updates

--- industry-verticals/luxury-retail/src/constants/localeOptions.ts
+++ industry-verticals/luxury-retail/src/constants/localeOptions.ts
@@ -4,4 +4,5 @@ export const localeOptions: LocaleOption[] = [
   { code: 'en', label: 'EN | USD', currency: 'USD', currencySymbol: '$' },
   { code: 'fr-FR', label: 'FR | EUR', currency: 'EUR', currencySymbol: '€' },
   { code: 'es-ES', label: 'ES | EUR', currency: 'EUR', currencySymbol: '€' },
+  { code: 'ar-SA', label: 'Arabic', currency: 'SAR', currencySymbol: 'ر.س' },
 ];
--- industry-verticals/luxury-retail/src/types/locale.ts
+++ industry-verticals/luxury-retail/src/types/locale.ts
@@ -1,4 +1,4 @@
-export type AppLocale = 'en' | 'fr-FR' | 'es-ES';
+export type AppLocale = 'en' | 'fr-FR' | 'es-ES' | 'ar-SA';
 
 export type LocaleOption = {
   code: AppLocale;
